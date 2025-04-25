c4341342f2e2b2277b979263ed443b47ddda6a46
nao-United92 <naoyauver@icloud.com>
2025-04-25 23:43:56 +0900

    ノートタイトルの更新処理の実装

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -23,13 +23,27 @@ const NoteDetail = () => {
     noteStore.set([note]);
     setIsLoading(false);
   };
+
+  const updateNote = async (
+    id: number,
+    note: { title?: string; content?: string }
+  ) => {
+    const updateNote = await noteRepository.update(id, note);
+    if (updateNote == null) return;
+    noteStore.set([updateNote]);
+    return updateNote;
+  };
+
   if (isLoading) return <div />;
   if (note == null) return <div>note is not existed</div>;
 
   return (
     <div className="pb-40 pt-20">
       <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
-        <TitleInput initialData={note} />
+        <TitleInput
+          initialData={note}
+          onTitleChange={(title) => updateNote(id, { title })}
+        />
       </div>
     </div>
   );
